157ad898ac47604ffbbd82a305ce419ee9d6a818
erlingrj <erling.jellum@gmail.com>
2024-01-08 01:43:18 +0100

    Update

--- src/img/opencv.cmake
+++ src/img/opencv.cmake
@@ -1,3 +1,3 @@
 find_package(OpenCV REQUIRED)
 include_directories(${OpenCV_INCLUDE_DIRS})
-target_link_libraries(${LF_TARGET_MAIN} ${OpenCV_LIBS})
+target_link_libraries(${LF_MAIN_TARGET} ${OpenCV_LIBS})
